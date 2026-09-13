b768b111dde3bdc558e036e69d7e73c9e9349540
felipegsantos9 <felipe.g.santos9@edu.senai.br>
2026-09-13 00:20:31 -0300

    Criação do Sistema de Fichas

---
+++ ficha/Thalia/script.js
@@ -0,0 +1,87 @@
+const entrada = document.getElementById("entrada");
+const entrar = document.getElementById("entrar");
+
+const musica = document.getElementById("musica");
+const controleMusica = document.getElementById("controle-musica");
+const site = document.getElementById("site");
+const topo = document.getElementById("topo");
+
+
+// ENTRAR NO SITE
+
+entrar.addEventListener("click", () => {
+
+    entrada.classList.add("saindo");
+
+    musica.volume = 0.25;
+
+    musica.play()
+        .then(() => {
+
+            controleMusica.textContent = "🔊";
+
+        })
+        .catch(() => {
+
+            console.log("A música não pôde ser iniciada.");
+
+        });
+
+});
+
+
+// CONTROLAR MÚSICA
+
+controleMusica.addEventListener("click", () => {
+
+    if (musica.paused) {
+
+        musica.play();
+
+        controleMusica.textContent = "🔊";
+
+    } else {
+
+        musica.pause();
+
+        controleMusica.textContent = "🔇";
+
+    }
+
+});
+
+
+// VOLTAR AO TOPO
+
+topo.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+
+// ANIMAÇÃO DA VIDA
+
+const progresso = document.querySelector(".progresso");
+
+const observador = new IntersectionObserver((entradas) => {
+
+    entradas.forEach((entrada) => {
+
+        if (entrada.isIntersecting) {
+
+            progresso.classList.add("animar");
+
+        }
+
+    });
+
+}, {
+    threshold: 0.5
+});
+
+
+observador.observe(progresso);
